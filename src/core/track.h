303e6b17b49135adc28a846c81383d053689063e
Kai Pastor <dg0yt@darc.de>
2018-11-02 05:35:00 +0100

    TrackPoint: Review special values, initialization
    Use NaN to indicate invalid/undefined floating point values.
    Move map_coord field to the end, add default member initializers,
    and remove the user-defined constructor. (However, continue to provide
    the special member functions for gcc 4, our Android compiler.)

--- src/core/track.h
+++ src/core/track.h
@@ -22,6 +22,7 @@
 #ifndef OPENORIENTEERING_TRACK_H
 #define OPENORIENTEERING_TRACK_H
 
+#include <cmath>
 #include <vector>
 
 #include <QDateTime>
@@ -39,20 +40,37 @@ namespace OpenOrienteering {
 
 
 /**
- * A point in a track or a waypoint, which stores position on ellipsoid and
- * map and more attributes.
+ * A geographic point with optional attributes such as time.
+ * 
+ * \see GPX `ptType`, https://www.topografix.com/GPX/1/1/#type_ptType
  */
 struct TrackPoint
 {
 	LatLon gps_coord;
+	QDateTime datetime;             // QDateTime() if invalid
+	float elevation     = NAN;      // NaN if invalid
+	float hDOP          = NAN;      // NaN if invalid
 	MapCoordF map_coord;
 	
-	QDateTime datetime;		// QDateTime() if invalid
-	float elevation;		// -9999 if invalid
-	float hDOP;				// -1 if invalid
+	// Default special member functions are fine.
+	// \todo Remove the special member functions after dropping Android gcc 4.9.
+#if __GNUC__ == 4
+	TrackPoint() = default;
+	TrackPoint(const TrackPoint&) = default;
+	TrackPoint(LatLon gps_coord,
+	           QDateTime datetime  = {},
+	           float elevation     = NAN,
+	           float hDOP          = NAN,
+	           MapCoordF map_coord = {} )
+	: gps_coord(gps_coord)
+	, datetime(datetime)
+	, elevation(elevation)
+	, hDOP(hDOP)
+	, map_coord(map_coord)
+	{}
+	~TrackPoint() = default;
+#endif
 	
-	TrackPoint(LatLon coord = LatLon(), const QDateTime& datetime = QDateTime(),
-			   float elevation = -9999, float hDOP = -1);
 	void save(QXmlStreamWriter* stream) const;
 };
 
